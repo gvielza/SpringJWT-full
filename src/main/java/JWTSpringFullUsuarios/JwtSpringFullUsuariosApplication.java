0001b0825081a9f997a42f66abdfa6ad215ba67b
gvielza <80008338+gvielza@users.noreply.github.com>
2022-08-12 16:08:37 -0300

    sistema de examenes con Spring y JWT

---
+++ src/main/java/JWTSpringFullUsuarios/JwtSpringFullUsuariosApplication.java
@@ -0,0 +1,13 @@
+package JWTSpringFullUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringFullUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringFullUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JWTSpringFullUsuarios/JwtSpringFullUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package JWTSpringFullUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringFullUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
